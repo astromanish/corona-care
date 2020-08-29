ba0693512f97e9026d743dae903ca8f8f1edef34
Manish Singh <astromanishsingh@gmail.com>
2020-08-30 00:42:59 +0530

    world overview in status also added

--- status.js
+++ status.js
@@ -14,13 +14,15 @@ const getData = () => {
 }
 
 const showData = (data) => {
-    console.log(Object.entries(data.data.regions))
+    console.log(data)
 
     const arrayRegions = Object.entries(data.data.regions);
-    const tbody = document.querySelector("#table-body");
+    const tbodyCountry = document.querySelector("#table-country-body");
+    const tbodySummary = document.querySelector("#table-overview-body");
     arrayRegions.forEach((cntry) => {
         const row = document.createElement("tr");
         const col1 = document.createElement("td");
+        col1.className = "text-dark"
         col1.appendChild(document.createTextNode(cntry[1].name));
         const col2 = document.createElement("td");
         col2.appendChild(document.createTextNode(cntry[1].total_cases));
@@ -34,8 +36,22 @@ const showData = (data) => {
         row.appendChild(col2);
         row.appendChild(col3);
         row.appendChild(col4);
-        tbody.appendChild(row);
+        tbodyCountry.appendChild(row);
     })
+
+    const row1 = document.createElement("tr");
+    const col5 = document.createElement("td");
+    col5.appendChild(document.createTextNode(data.data.summary.total_cases));
+    const col6 = document.createElement("td");
+    col6.className = "text-success";
+    col6.appendChild(document.createTextNode(data.data.summary.recovered));
+    const col7 = document.createElement("td");
+    col7.className = "text-danger";
+    col7.appendChild(document.createTextNode(data.data.summary.deaths));
+    row1.appendChild(col5);
+    row1.appendChild(col6);
+    row1.appendChild(col7);
+    tbodySummary.appendChild(row1);
 
 }
 
