07f440528d9b24f06d72f24553513452790efbea
Manish Singh <astromanishsingh@gmail.com>
2020-10-07 19:58:15 +0530

    world stat styled

--- src/components/worldStat/countryData.js
+++ src/components/worldStat/countryData.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import axios from "axios";
-import { Link } from 'react-router-dom';
 import NumberFormat from 'react-number-format';
 
 class CountryData extends Component {
