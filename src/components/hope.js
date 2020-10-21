eb8074caa1f63381d16b752ae2e76d15d0742ad9
Manish Singh <astromanishsingh@gmail.com>
2020-10-21 20:01:17 +0530

    amogh data updated with html and css refactored

--- src/components/hope.js
+++ src/components/hope.js
@@ -1,10 +1,61 @@
-import React from 'react'
+import React from 'react';
+import './css/hope.css';
 
 const Hope = () => {
     return (
-        <div>
-            to be added..(Hope)
-        </div>
+        // <>
+        //     <div className="container-fluid">
+        //         <div className="row">
+        //             <div className="flex-box">
+        //                 <div id="myCarousel" className="carousel slide" data-ride="carousel">
+        //                     {/* Indicators */}
+        //                     <ol className="carousel-indicators">
+        //                         <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
+        //                         <li data-target="#myCarousel" data-slide-to="1"></li>
+        //                         <li data-target="#myCarousel" data-slide-to="2"></li>
+        //                         <li data-target="#myCarousel" data-slide-to="3"></li>
+        //                     </ol>
+
+        //                     {/* Wrapper for slides */}
+        //                     <div className="carousel-inner">
+        //                         <div className="item active">
+        //                             <iframe width="100%" height="345" src="https://www.youtube.com/embed/U8r3oTVMtQ0">
+        //                             </iframe>
+        //                         </div>
+
+        //                         <div className="item">
+        //                             <iframe width="100%" height="345" src="https://www.youtube.com/embed/2JsWf-2nN1Y">
+        //                             </iframe>
+        //                         </div>
+
+        //                         <div className="item">
+        //                             <iframe width="100%" height="345" src="https://www.youtube.com/embed/xTvd7oAEyhs">
+        //                             </iframe>
+        //                         </div>
+
+        //                         <div className="item">
+        //                             <iframe width="100%" height="345" src="https://www.youtube.com/embed/S3d66JvDYvk">
+        //                             </iframe>
+        //                         </div>
+        //                     </div>
+
+        //                     {/* Left and right controls */}
+        //                     <a className="left carousel-control" href="#myCarousel" data-slide="prev">
+        //                         <span className="glyphicon glyphicon-chevron-left"></span>
+        //                         <span className="sr-only">Previous</span>
+        //                     </a>
+        //                     <a className="right carousel-control" href="#myCarousel" data-slide="next">
+        //                         <span className="glyphicon glyphicon-chevron-right"></span>
+        //                         <span className="sr-only">Next</span>
+        //                     </a>
+        //                 </div>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </>
+        <>
+
+        </>
     );
 }
 
